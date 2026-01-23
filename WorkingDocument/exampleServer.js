cc9aa296a0340530738f7326ca183c7a665d8c5b
tomortimer <tcmortimer@outlook.com>
2026-01-23 20:21:35 +0000

    working on data comms and ejs

--- WorkingDocument/exampleServer.js
+++ WorkingDocument/exampleServer.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 8080;
 
+//load static resources
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+
+//load dynamic resources
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//get request for homepage
 app.get('/', (req, res) => {
-  res.send('Hello World from Express.js!');
 });
+
+app.post('/login', (req, res) => {
+  //access query parameters
+  const user = req.body[userID];
+
+  res.render('user', {
+    title: 'Express Template Example',
+    message: 'Hello user ${user}!',
+    items: ['Item 1', 'Item 2', 'Item 3']
+  });
+})
+
+
 
 app.listen(port, () => {
   console.log(`Express server running at http://localhost:${port}`);
