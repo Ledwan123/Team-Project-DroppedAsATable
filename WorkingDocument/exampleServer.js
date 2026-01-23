a870f1de7633a66f315c210e380ed95230b44851
tomortimer <tcmortimer@outlook.com>
2026-01-23 15:42:54 +0000

    NodeJS app using Express module accessible from Local Network

--- WorkingDocument/exampleServer.js
+++ WorkingDocument/exampleServer.js
@@ -1,16 +1,11 @@
-const { createServer } = require('node:http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-    var data = fs.readFileSync("index.html");
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data);
+app.get('/', (req, res) => {
+  res.send('Hello World from Express.js!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
 });
