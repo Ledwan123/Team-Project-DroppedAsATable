5445a16a83e07948c5d71d402544ddb13162e587
sryoma <rws211@exeter.ac.uk>
2026-01-25 23:59:26 +0000

    added other tables to database based on erd

--- WorkingDocument/database_methods.py
+++ WorkingDocument/database_methods.py
@@ -10,17 +10,15 @@ class DatabaseMethods:
 
         #table to store users, if anyone knows anything about password security stuff we could do that instead of storing plaintext
         #also, usertype enum is short for travellers, admins, maintainers as said in the spec
-        cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY, userName TEXT, password TEXT,userType TEXT CHECK(userType in ('T','A','M')))")
-
-        #could create all the other tables (node, mission etc)in here too if we want them all in one .db
+        cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY, userName TEXT, password TEXT,userType TEXT CHECK(userType in ('T','A','M')), points INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS missions(missionID INTEGER PRIMARY KEY, question TEXT, answer TEXT)") #will change missions depending on how we deal with representing/storing routes
+        cursor.execute("CREATE TABLE IF NOT EXISTS changes(changeID INTEGER PRIMARY KEY, userID INTEGER, missionID INTEGER, time TEXT, FOREIGN KEY(userID) REFERENCES users(userID), FOREIGN KEY(missionID) REFERENCES missions(missionID))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS nodes(nodeID INTEGER PRIMARY KEY, coordinatesX INTEGER, coordinatesY INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS locations(locationID INTEGER PRIMARY KEY, name TEXT, nodeID INTEGER, type TEXT, FOREIGN KEY(nodeID) REFERENCES nodes(nodeID))") #type will be used if we want to display locations with icons on the map e.g station type with a small train image etc...
+        cursor.execute("CREATE TABLE IF NOT EXISTS edges(edgeID INTEGER PRIMARY KEY, startNode INTEGER, endNode INTEGER, length INTEGER, lighting INTEGER, FOREIGN KEY(startNode) REFERENCES nodes(nodeID), FOREIGN KEY(endNode) REFERENCES nodes(nodeID))")#other indicators will be added here depending on what we decide on
 
         return()
 
     def addUser(self,username,password,usertype):
         cursor=self.connection.cursor()
         cursor.execute("INSERT INTO users (userID,userName,password,userType) VALUES (?,?,?,?)",(None, username, password, usertype))
-        
-
-a=DatabaseMethods()
-a.setup()
-a.addUser("john_database","password123","T")
