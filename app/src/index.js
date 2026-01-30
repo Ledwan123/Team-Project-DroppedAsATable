185fd57e48dda1aa99f016b8e1b708edfa80cd4d
Carpenter-Sam <109752824+Carpenter-Sam@users.noreply.github.com>
2026-01-30 11:04:08 +0000

    Node Install

---
+++ app/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/home.html");
+
+app.get("/", (req, res) => {
+
+    fs.readFile("app/views/layouts/home.html", "utf8", (err, html) => {
+        if (err) {
+            res.status(500).send("INTERNAL SERVER ERROR");
+        }
+        
+        res.send(html);
+    })
+
+});
+
+
+app.listen(process.env.PORT || 3000, () => console.log("App available on http://localhost:3000"));
