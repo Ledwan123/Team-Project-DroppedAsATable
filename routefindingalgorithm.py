e29fc6d0f3fa4cc745083f1cda79fcfcd2c6dacd
BenJ729 <bhmj201@exeter.ac.uk>
2026-02-02 06:24:43 -0800

    Update routefindingalgorithm.py
    finding other routes improvements

--- routefindingalgorithm.py
+++ routefindingalgorithm.py
@@ -4,17 +4,35 @@ import random
 segments = [
     ("A", "B", 5, 3, 6),
     ("B", "C", 2, 2, 5),
-    ("A", "C", 1, 1, 8)
+    ("A", "C", 1, 1, 8),
+    ("C", "D", 1, 5, 2),
+    ("D", "E", 5, 2, 4),
+    ("E", "F", 5, 3, 6),
+    ("F", "G", 5, 4, 7),
+    ("G", "C", 5, 2, 3),
+    ("H", "F", 5, 1, 5),
+    ("C", "J", 5, 3, 6),
+    ("B", "D", 4, 4, 4),
+    ("C", "E", 6, 2, 5),
+    ("D", "F", 3, 3, 7)
+
 ]
 
 nodes = [
     ("A", 0, 0),
     ("B", 5, 0),
-    ("C", 9, 0)
+    ("C", 9, 0),
+    ("D", 10, 0),
+    ("E", 15, 0),
+    ("F", 20, 0),
+    ("G", 25, 0),
+    ("H", 30, 0),
+    ("I", 35, 0),
+    ("J", 40, 0)
 ]
 
 
-whereRouting = ("A", "C")
+whereRouting = ("A", "J")
 
 def findRoute(segments, nodes, whereRouting, weightings=None):
 
@@ -72,13 +90,12 @@ def findRoute(segments, nodes, whereRouting, weightings=None):
 def findOtherRoutes(segments, nodes, whereRouting, firstRoute, weightings = [1, 0, 0]):
     escapeCounter = 0
     while escapeCounter < 1000:
-        whichweight = random.randrange(0, 2)
+        whichweight = random.randrange(0, 3)
         howmuch = random.choice([-1, 1])
         weightings[whichweight] += howmuch
-        route = findRoute(segments, nodes, whereRouting, [3, 2, 4])
-
-        similarity = len(set(firstRoute) and set(route)) / float(len(set(firstRoute) or set(route))) * 100
-        if similarity < 70:
+        route = findRoute(segments, nodes, whereRouting, weightings)
+        similarity = len(set(route[whereRouting[1]][1:]).difference(set(firstRoute[whereRouting[1]][1:])))/len(route[whereRouting[1]][1:]) * 100
+        if similarity > 0:
             return route
 
         escapeCounter += 1
@@ -88,4 +105,4 @@ def findOtherRoutes(segments, nodes, whereRouting, firstRoute, weightings = [1, 
 
 abba = findRoute(segments, nodes, whereRouting)
 print(abba)
-#print(findOtherRoutes(segments, nodes, whereRouting, abba))
+print(findOtherRoutes(segments, nodes, whereRouting, abba))
