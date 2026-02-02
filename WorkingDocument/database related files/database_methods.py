e1137551fba619a5c6fa3ce37082eea3acfe3273
sryoma <rws211@exeter.ac.uk>
2026-02-02 15:08:45 +0000

    added new indicators to database and related methods

--- WorkingDocument/database related files/database_methods.py
+++ WorkingDocument/database related files/database_methods.py
@@ -10,27 +10,27 @@ class DatabaseMethods:
         
         #table to store users, if anyone knows anything about password security stuff we could do that instead of storing plaintext
         cursor.execute("CREATE TABLE IF NOT EXISTS nodes(nodeID INTEGER PRIMARY KEY, coordinatesX REAL, coordinatesY REAL)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY, userName TEXT, email TEXT, password TEXT,userType TEXT CHECK(userType in ('T','A','M')), points INTEGER, lengthWeight REAL, lightingWeight REAL)") # usertype enum is short for travellers, admins, maintainers as said in the spec
+        cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY, userName TEXT, email TEXT, password TEXT,userType TEXT CHECK(userType in ('T','A','M')), points INTEGER, lengthWeight REAL, lightingWeight REAL, crimeWeight REAL, trafficWeight REAL, greeneryWeight REAL, gradientWeight)") # usertype enum is short for travellers, admins, maintainers as said in the spec
         cursor.execute("CREATE TABLE IF NOT EXISTS missions(missionID INTEGER PRIMARY KEY, question TEXT, startNode INTEGER, endNode INTEGER, FOREIGN KEY(startNode) REFERENCES nodes(nodeID), FOREIGN KEY(endNode) REFERENCES nodes(nodeID))") #will change missions depending on how we deal with representing/storing routes
         cursor.execute("CREATE TABLE IF NOT EXISTS changes(changeID INTEGER PRIMARY KEY, userID INTEGER, missionID INTEGER, time TEXT, FOREIGN KEY(userID) REFERENCES users(userID), FOREIGN KEY(missionID) REFERENCES missions(missionID))")
         cursor.execute("CREATE TABLE IF NOT EXISTS locations(locationID INTEGER PRIMARY KEY, name TEXT, nodeID INTEGER, locationType TEXT, FOREIGN KEY(nodeID) REFERENCES nodes(nodeID))") #type will be used if we want to display locations with icons on the map e.g station type with a small train image etc...
-        cursor.execute("CREATE TABLE IF NOT EXISTS edges(edgeID INTEGER PRIMARY KEY, startNode INTEGER, endNode INTEGER, length INTEGER, lighting INTEGER, FOREIGN KEY(startNode) REFERENCES nodes(nodeID), FOREIGN KEY(endNode) REFERENCES nodes(nodeID))")#other indicators will be added here depending on what we decide on
+        cursor.execute("CREATE TABLE IF NOT EXISTS edges(edgeID INTEGER PRIMARY KEY, startNode INTEGER, endNode INTEGER, length REAL, lighting REAL, crime REAL, traffic REAL, greenery REAL, gradient REAL, FOREIGN KEY(startNode) REFERENCES nodes(nodeID), FOREIGN KEY(endNode) REFERENCES nodes(nodeID))")#other indicators will be added here depending on what we decide on
 
         return()
     
     #methods used by route finding##
     def getUserWeights(self, userID):
         cursor=self.connection.cursor()
-        cursor.execute("SELECT lengthWeight, lightingWeight FROM users WHERE userID = ?", (userID))
+        cursor.execute("SELECT lengthWeight, lightingWeight, crimeWeight, trafficWeight, greeneryWeight, gradientWeight FROM users WHERE userID = ?", (userID))
         return(cursor.fetchall())
 
     def setUserWeights(self,userID, weights):
         cursor=self.connection.cursor()
-        cursor.execute("UPDATE users SET lengthWeight=?, lightingWeight=? WHERE userID = ?",(weights[0],weights[1],userID))
+        cursor.execute("UPDATE users SET lengthWeight=?, lightingWeight=?,crimeWeight=?, trafficWeight=?, greeneryWeight=?, gradientWeight=? WHERE userID = ?",(weights[0],weights[1],weights[2],weights[3],weights[4],weights[5],userID))
 
-    def getSurroundingNodes(self,node):    #returns neighboring nodes and their indicators (indicators will be updated once we decide on which we are using)
+    def getSurroundingNodes(self,node):    #returns neighboring nodes and their indicators
         cursor=self.connection.cursor()
-        cursor.execute("SELECT startNode, length,lighting FROM edges WHERE endNode = ? UNION SELECT endNode, length,lighting FROM edges WHERE startNode = ?",(node,node))
+        cursor.execute("SELECT startNode, length,lighting,crime,traffic,greenery,gradient FROM edges WHERE endNode = ? UNION SELECT endNode, length,lighting,crime,traffic,greenery,gradient FROM edges WHERE startNode = ?",(node,node))
         return(cursor.fetchall())
     ################################
 
@@ -39,9 +39,9 @@ class DatabaseMethods:
         cursor=self.connection.cursor()
         cursor.execute("INSERT INTO nodes (nodeID, coordinatesX,coordinatesY) VALUES (?,?,?)",(None, coordinatesX,coordinatesY))
 
-    def addEdge(self,startNode,endNode,length,lighting):
+    def addEdge(self,startNode,endNode,length,lighting,crime,traffic,greenery,gradient):
         cursor=self.connection.cursor()
-        cursor.execute("INSERT INTO edges(edgeID,startNode,endNode,length,lighting) VALUES(?,?,?,?,?)",(None,startNode,endNode,length,lighting))
+        cursor.execute("INSERT INTO edges(edgeID,startNode,endNode,length,lighting,crime,traffic,greenery,gradient) VALUES(?,?,?,?,?)",(None,startNode,endNode,length,lighting))
 
     def addLocation(self,name,nodeID,locationType):
         cursor=self.connection.cursor()
@@ -85,9 +85,10 @@ class DatabaseMethods:
     #login methods##################
     def addUser(self,username, email, password,usertype): #used when a user chooses to sign up and make an account
         cursor=self.connection.cursor()
-        cursor.execute("INSERT INTO users (userID,userName,email,password,userType,points,lengthWeight,lightingWeight) VALUES (?,?,?,?,?,?,?,?)",(None, username, email, password, usertype,0,1,1))
+        cursor.execute("INSERT INTO users (userID,userName,email,password,userType,points,lengthWeight,lightingWeight,crimeWeight, trafficWeight, greeneryWeight, gradientWeight) VALUES (?,?,?,?,?,?,?,?)",(None, username, email, password, usertype,0,1,1,1,1,1,1))
 
     def getLoginDetails(self, username, email):  #given the username and email, returns passwords
         cursor=self.connection.cursor()
         cursor.execute("SELECT password FROM users WHERE username = ? AND email = ?",(username, email))
+
     #################################
