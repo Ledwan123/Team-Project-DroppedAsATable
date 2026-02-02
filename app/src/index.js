28aa7e379c0d1a2f0fd0e169c6b9943808639425
Carpenter-Sam <109752824+Carpenter-Sam@users.noreply.github.com>
2026-02-02 12:51:40 +0000

    Server now correctly serves statis files via Express

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
 
 const app = express();
+app.use('/static', express.static(path.join('/workspaces/Team-Project-DroppedAsATable/app/', 'public')));
 
 app.get("/home.html");
 
 app.get("/", (req, res) => {
 
-    fs.readFile("/workspaces/Team-Project-DroppedAsATable/app/views/home.html", "utf8", (err, html) => {
+    fs.readFile("/workspaces/Team-Project-DroppedAsATable/app/views/login.html", "utf8", (err, html) => {
         if (err) {
             res.status(500).send("INTERNAL SERVER ERROR");
         }
@@ -16,5 +18,30 @@ app.get("/", (req, res) => {
     })
 
 });
+
+app.get("/signup.html", (req, res) => {
+
+    fs.readFile("/workspaces/Team-Project-DroppedAsATable/app/views/signup.html", "utf8", (err, html) => {
+        if (err) {
+            res.status(500).send("INTERNAL SERVER ERROR");
+        }
+        
+        res.send(html);
+    })
+
+});
+
+app.get("/login.html", (req, res) => {
+
+    fs.readFile("/workspaces/Team-Project-DroppedAsATable/app/views/login.html", "utf8", (err, html) => {
+        if (err) {
+            res.status(500).send("INTERNAL SERVER ERROR");
+        }
+        
+        res.send(html);
+    })
+
+});
+
 
 app.listen(process.env.PORT || 3000, () => console.log("App available on http://localhost:3000"));
