4e6704f2d63c0ce4b3677c79c4c3a35805ec4f37
Tom Mortimer <tcmortimer@outlook.com>
2026-02-04 22:15:18 +0000

    fixed html pages rendering with flask

--- WorkingDocument/flask stuff/server.py
+++ WorkingDocument/flask stuff/server.py
@@ -1,21 +1,29 @@
-from flask import render_template, Flask, request
+from flask import render_template, Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return redirect('/login')
 
-@app.route("/login.html", methods=["GET", "POST"])
-def index():
+@app.route("login.html")
+def login_redirect():
+    return redirect('login')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "GET":
-        return render_template("home.html")
+        return render_template("login.html")
     if request.method == "POST":
         # Check with database
         pass
 
-@app.route("/signup.html", methods=["GET", "POST"])
-def index():
+@app.route("signup.html")
+def signup_redirect():
+    return redirect('signup')
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
