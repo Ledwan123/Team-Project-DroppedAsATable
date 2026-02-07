ae5cd61494ad4a0e96242f4c9528054833b300b0
Ledwan123 <lucaciuedward@gmail.com>
2026-02-07 22:59:33 +0000

    Update demo.py to include new db changes and better frontend handling

--- WorkingDocument/MapStuff/DemoMap/demo.py
+++ WorkingDocument/MapStuff/DemoMap/demo.py
@@ -3,7 +3,7 @@ import sqlite3
 from database_methods import *
 
 app = Flask(__name__)
-
+#lighting, greenery, elevation, crime, distance
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,19 +18,42 @@ def index():
 @app.route("/addnode", methods=["POST"])
 def add_node():
     data = request.get_json()
-
     myDatabase = DatabaseMethods()
-    myDatabase.setup()
 
     node_id = data["id"]
     coordx = data["coordx"]
     coordy = data["coordy"]
-
-    myDatabase.addNode(coordx, coordy)
-
+    lighting = data["lighting"]
+    crime = data["crime"]
+    greenery = data["greenery"]
+    gradient = data["gradient"]
+    myDatabase.addNode(coordx, coordy, lighting, crime, greenery, gradient)
+    
     print(myDatabase.getMapData())
+    nodes, edges = myDatabase.getMapData()
+    
+    myDatabase.closeConnection()
+    return jsonify({"status": "ok", "nodes": nodes, "edges": edges})
+    
 
-    return jsonify({"status": "ok"})
+@app.route("/addsegment", methods=["POST"])
+def add_segment():
+    data = request.get_json()
+    myDatabase = DatabaseMethods()
+
+    segment_id = data["id"]
+    start_node = data["startNode"]
+    end_node = data["endNode"]
+    length = data["length"]
+    myDatabase.addEdge(start_node, end_node, length)
+    nodes, edges = myDatabase.getMapData()
+    
+    myDatabase.closeConnection()
+    return jsonify({"status": "ok", "nodes": nodes, "edges": edges})
+   
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    myDatabase = DatabaseMethods()
+    myDatabase.setup()
+    myDatabase.closeConnection()
+    app.run(debug=False)
