d527fc82e0984e7ea4d77f4e6f965b803a44ad21
BenJ729 <bhmj201@exeter.ac.uk>
2026-02-11 10:55:54 -0800

    fixed function to work with dictionary of nodes

--- WorkingDocument/database related files/routefindingtests.py
+++ WorkingDocument/database related files/routefindingtests.py
@@ -1,28 +1,28 @@
 import routefindingalgorithm
 
 segments = [
-    ("21288399", "B", 5),
+    ("21289", "B", 5),
     ("B", "C", 2),
-    ("21288399", "C", 1),
+    ("21289", "C", 1),
     ("C", "D", 1),
-    ("D", "1978476803", 5),
-    ("E", "F", 5),
+    ("D", "1978476", 5),
+    ("1978476", "F", 5),
     ("F", "G", 5),
     ("G", "C", 5),
     ("H", "F", 5),
     ("C", "J", 5),
     ("B", "D", 4),
-    ("C", "1978476803", 6),
+    ("C", "1978476", 6),
     ("D", "F", 3)
 
 ]
 
 nodes = {
-    "21288399" : (0, 0, 4, 3),
+    "21289" : (0, 0, 4, 3),
     "B": (5, 0, 5 , 4),
     "C": (9, 0, 3, 5),
     "D": (10,4 ,4, 4),
-    "1978476803": (15, 0, 4,8),
+    "1978476": (15, 0, 4,8),
     "F": (20, 0,6,5),
     "G": (25, 0,4,3),
     "H": (30, 0,3,7),
@@ -31,10 +31,10 @@ nodes = {
 }
 
 
-whereRouting = ("21288399", "1978476803")
+whereRouting = ("21289", "1978476")
 
 
-#abba = routefindingalgorithm.findRoute(segments, nodes, whereRouting)
-#print(abba)
-#print(routefindingalgorithm.findOtherRoutes(segments, nodes, whereRouting, [abba[whereRouting[1]]]))
-print(routefindingalgorithm.findMultipleRoutes())
+abba = routefindingalgorithm.findRoute(segments, nodes, whereRouting)
+print(abba)
+print(routefindingalgorithm.findOtherRoutes(segments, nodes, whereRouting, [abba[whereRouting[1]]]))
+#print(routefindingalgorithm.findMultipleRoutes())
