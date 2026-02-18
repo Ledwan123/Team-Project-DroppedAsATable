0a8779110ea01d1edb07cd2973a1d5517ac68f71
tomortimer <tcmortimer@outlook.com>
2026-02-18 16:14:24 +0000

    new routefinder, db is struggling with Sunday's again

--- demo/routefindingalgorithm.py
+++ demo/routefindingalgorithm.py
@@ -13,18 +13,18 @@ def findRoute(segments, nodes, whereRouting, weightings=None):
     if weightings:
         for segment in segments: #apply weightings to each segment
             segmentid, start, end, length = segment
-            weight = length * weightings[0] *2
+            weight = length * (weightings[0]**3) *2
             weightingIterator = 1
             for node in nodes:
                 if node[0] == start:
                     for tempWeight in node[1:]:
-                        weight += float(tempWeight)*float(weightings[weightingIterator])*length
+                        weight += float(tempWeight)*(float(weightings[weightingIterator])**3)*length
                         weightingIterator+=1
             weightingIterator = 1
             for node in nodes:
                 if node[0] == end:
                     for tempWeight in node[1:]:
-                        weight += float(tempWeight)*float(weightings[weightingIterator])*length
+                        weight += float(tempWeight)*(float(weightings[weightingIterator])**3)*length
                         weightingIterator+=1
             weightedSegments.append((start, end, weight))
     else:
@@ -33,12 +33,16 @@ def findRoute(segments, nodes, whereRouting, weightings=None):
             weight = length
             weightedSegments.append((start, end, weight))
 
-    distMatrix = numpy.zeros((len(nodes),len(nodes)))
+    #create numpy matrix the size of the amount of nodes
+    distmatrixSize = int(nodes[-1][0])
+    distMatrix = numpy.zeros((len(nodes), len(nodes)))
     
+    #add the segments to the numpy array
     for segment in weightedSegments:
         distMatrix[int(segment[0])][int(segment[1])] = segment[2]
         distMatrix[int(segment[1])][int(segment[0])] = segment[2]
     
+    #use scipy dijkstras implementation to get distance and pred matrix
     distances, pred = scipy.sparse.csgraph.dijkstra(distMatrix, return_predecessors=True)
 
     return distances, pred
@@ -48,7 +52,7 @@ def findRoute(segments, nodes, whereRouting, weightings=None):
 
 
 
-def findOtherRoutes(segments, nodes, whereRouting, routes, weightings = [1, 0, 0, 0, 0], seed = 0, similarityNeeded = 20):
+def findOtherRoutes(segments, nodes, whereRouting, routes, weightings = [1, 0, 0, 0, 0], seed = 0, similarityNeeded = 20, removingNodes = False):
     escapeCounter = 0 #escape counter to set max iterations so does not loop forever
 
     # calculate the total of weightings so that when the weights are adjusted it adjusts them by an apropriate amount
@@ -57,36 +61,35 @@ def findOtherRoutes(segments, nodes, whereRouting, routes, weightings = [1, 0, 0
         weightingsMagnitude += weight
 
 
-    while escapeCounter < 50:
+    while escapeCounter < 10:
 
-        #temp values for the weights that are changing to check a weight never goes below 0
-        changingWeight1 = -1
-        changingWeight2 = 1
-
-        while changingWeight1 < 0 or changingWeight2 < 0:
-
-            #calculate which weight is changing and by how much 
-            random.seed(seed)
-            whichweight = random.randrange(0, len(weightings))
-            random.seed(seed)
-            howmuch = random.uniform(0, weightingsMagnitude)
-            changingWeight1 = weightings[whichweight] + howmuch
-
-            #loop used to iterate seed until a weighting to subtract the weighting from is found
-            i = whichweight
-            while i == whichweight:
-                seed += 1
-                random.seed(seed)
-                i = random.randrange(0, len(weightings))
-            changingWeight2 = weightings[i] - howmuch
-        weightings[whichweight] = changingWeight1
-        weightings[i] = changingWeight2
-
+        #randomise weightings to create the other routes
         weightings = []
         for x in range(5):
             weightings.append(random.uniform(0, weightingsMagnitude))
             seed+=1
+
+
+        #start removing edges to force a different path
+        if removingNodes == True:
             random.seed(seed)
+            for singleRoute in routes:
+                toRemove = random.choice(singleRoute)
+                exitready = False
+                while exitready == False:
+                    for x in range(len(segments)):
+                            if segments[x][1] == toRemove:
+                                segments.pop(x)
+                                break
+                            if segments[x][2] == toRemove:
+                                segments.pop(x)
+                                break
+                    exitready = True
+                    seed+=1
+                        
+                    
+
+
 
         #attempt to find a different route with the new adjusted weightings 
         routeweights, routePr = findRoute(segments, nodes, whereRouting, weightings)
@@ -121,6 +124,7 @@ def findMultipleRoutes(whereRouting,userID = 1, numberOfRoutes = 3):
     weightingstemp = weightingstemp[0]
     myDatabase.closeConnection()
 
+
     routes = []
     weightings = []
 
@@ -139,17 +143,27 @@ def findMultipleRoutes(whereRouting,userID = 1, numberOfRoutes = 3):
     iterator = 0
     while len(routes) < numberOfRoutes and iterator<3:
         newRoute, seed = findOtherRoutes(segments, nodes, whereRouting, routes, seed = seed)
-        print(newRoute)
         if newRoute:
-            print(newRoute)
             routes.append(newRoute)
         iterator += 1
-    print("AAAA", routes)
+
+    #start removing nodes to find more routes
+    if len(routes) < numberOfRoutes:
+        iterator = 0
+        while len(routes) < numberOfRoutes and iterator<20:
+            tempSegments = segments
+            newRoute, seed = findOtherRoutes(tempSegments, nodes, whereRouting, routes, seed = seed, removingNodes = True)
+            if newRoute:
+                routes.append(newRoute)
+            seed += 1
+            iterator += 1
+
+
     while len(routes) < numberOfRoutes:
         routes.append(routes[0])
     return routes
 
-
+#get the path from the priors matrix
 def getPath(Pr,i,j):
     path = [int(j)]
     k = j
